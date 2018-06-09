dbb7e07b2222c37ccea8aa95ef119c5b9a18c849
Andreas Schreiner <schreiner.andreas@outlook.com>
2018-06-09 14:31:27 +0200

    up

---
+++ SqlitePaswordSetter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
+using de.fearvel.net.SQL.Connector;
+namespace de.fearvel.gui.SqlitePaswordSetter
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPathFinder_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dia = new OpenFileDialog();
+            if (dia.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBoxPath.Text = dia.FileName;
+            }
+        }
+
+        private void ButtonSet_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxPath.Text.Length == 0)
+            {
+                MessageBox.Show("Please select a File", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var con = new SqliteConnector(TextBoxPath.Text, TextBoxOldPassword.Text);
+                con.SetPassword(TextBoxNewPassword.Text);
+                MessageBox.Show("Done", "OK", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("An Error Occured, the Password couldnt be changed\n" + exception, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
